e927757ebe15757b218a5f46615bcd26d7784d82
U-JoelsLaptop\Joel <Joel@JoelsLaptop.(none)>
2016-12-26 08:33:04 -0800

    no body parser

--- app.js
+++ app.js
@@ -2,15 +2,9 @@ var express = require("express");
 
 var app = express();
 
-var bodyParse = require("body-parser");
-
 var mainPage = require("mainPage.js");
 
-
 var apiRoot = "/";
-
-app.use(bodyParse.json());
-
 
 mainPage.register(app, apiRoot);
 
