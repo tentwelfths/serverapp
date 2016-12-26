ba0d790ea3ddf03653eafc65376d4c8312aea163
U-JoelsLaptop\Joel <Joel@JoelsLaptop.(none)>
2016-12-26 09:26:28 -0800

    registering a user???

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ var express = require("express");
 var app = express();
 
 var mainPage = require("./mainPage.js");
+var users = require("./users.js");
+var database = require("./db.js");
 
 var apiRoot = "/";
 
+database.start();
+
 mainPage.register(app, apiRoot);
+mainPage.register(app, apiRoot + "users/");
 
 app.listen(8080);
 
